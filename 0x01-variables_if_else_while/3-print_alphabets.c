db0562372becb4a0f1868684219b8ef3386a63b0
habtamubaharu29 <habahar20@gmail.com>
2023-10-09 07:43:03 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,10 +15,9 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
-	for (low = 'A'; low<= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
 	putchar('\n');
 
 	return (0);
 }
-	
