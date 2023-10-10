fca388794765839b98a7d718138f057af64414f9
habtamubaharu29 <habahar20@gmail.com>
2023-10-10 14:34:28 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,17 +9,17 @@ int main(void)
 
 {
 
-	int i, z = 0;
+        int i, z = 0;
 
-	while (i < 1024);
-	{
-	if ((i % 3 == 0) || (i % == 0))
-	{
-	z += i;
-	}
-	i++;
-	}
-	printf("%d\n, z");
-	return (0);
+        while (i < 1024);
+        {
+        if ((i % 3 == 0) || (i % == 0))
+        {
+        z += i;
+        }
+        i++;
+        }
+        printf("%d\n, z");
+        return (0);
 
 }
