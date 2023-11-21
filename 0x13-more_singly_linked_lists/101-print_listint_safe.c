811dabb78116fc47f75f8fc71698b918d109228d
habtamubaharu29 <habahar20@gmail.com>
2023-11-20 23:59:51 -0800

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,14 +15,15 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		diff = head - head->next;
 		num++;
-		printf("[%p] %d\n", (void *)head, head->);
+		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %d\n, (void)head->, head->next->n);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
 	}
+
 	return (num);
 }
