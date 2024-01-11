66d22b7e65e7ad15b0e9944d73c5c56fd71d6348
habtamubaharu29 <habahar20@gmail.com>
2024-01-11 06:35:46 -0800

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,16 +10,20 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum;
+
 	sum = 0;
+
 	if (head != NULL)
 	{
 		while (head->prev != NULL)
 			head = head->prev;
+
 		while (head != NULL)
 		{
 			sum += head->n;
 			head = head->next;
 		}
 	}
+
 	return (sum);
 }
